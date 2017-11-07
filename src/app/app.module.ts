22b883a620842e5636574c618a11e1eee49f227d
steve zeitoun <stevez@MacBook-Pro-de-steve.local>
2017-11-07 17:19:20 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'about',
+        pathMatch: 'full'
+        },
+      {
+      path: 'about',
+      component: AboutComponent,
+      },
+      {
+      path: 'posts',
+      component:  PostsComponent,
+      },
+
+      ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
